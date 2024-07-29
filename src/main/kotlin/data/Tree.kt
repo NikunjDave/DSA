3e0edf8236a661f35e21ffb593a612b67469482d
NikunjDave <nikk.dave@gmail.com>
2024-07-29 11:03:45 +0530

    solve tree diameter problem

--- src/main/kotlin/data/Tree.kt
+++ src/main/kotlin/data/Tree.kt
@@ -1,5 +1,7 @@
 package data
 
+import java.util.LinkedList
+
 class TreeNode<T>(var data: T) {
     var left : TreeNode<T>?  = null
     var right : TreeNode<T>?  = null
@@ -21,4 +23,37 @@ fun createTree(): TreeNode<Int>{
     node.right?.right?.left = TreeNode(9)
     node.right?.right?.right = TreeNode(10)
     return node
+}
+
+fun <T>createTree(nodeList : List<T>) : TreeNode<T>?{
+    if(nodeList.isEmpty())
+        return null
+    // Create the root node of the binary tree
+    val  root : TreeNode<T> = TreeNode(nodeList[0])
+    val queue = LinkedList<TreeNode<T>>()
+    queue.add(root)
+    // Start iterating over the list of listOfNodes starting from the second node
+    var i = 1
+    while(i < nodeList.size){
+        // Get the next node from the queue
+        val curr = queue.remove()
+
+        // If the node is not null, create a new TreeNode object for its left child,
+        // set it as the left child of the current node, and add it to the queue
+        if(nodeList[i] != null){
+            curr.left = TreeNode(nodeList[i])
+            queue.add(curr.left!!)
+        }
+        i++
+        // If there are more listOfNodes in the list and the next node is not null,
+        // create a new TreeNode object for its right child, set it as the right child
+        // of the current node, and add it to the queue
+        if(i < nodeList.size && nodeList[i] != null){
+            curr.right = TreeNode(nodeList[i])
+            queue.add(curr.right!!)
+        }
+        i++
+    }
+
+    return root
 }
--- src/main/kotlin/tree/DiameterOfBinaryTree.kt
+++ src/main/kotlin/tree/DiameterOfBinaryTree.kt
@@ -1,19 +1,31 @@
 package tree
 
 import data.TreeNode
+import data.createTree
 
 /**
  * The diameter of binary tree is the longest path between
  * two nodes
  */
 class DiameterOfBinaryTree {
-
-    fun findDiameter(root : TreeNode<Int>){
-
+    private var ans = 0
+    fun findDiameter(root : TreeNode<Int>?) : Int{
+        maxDepth(root)
+        return ans
     }
 
-
-    fun buildTree(){
-        
+    private fun maxDepth(node : TreeNode<Int>?) : Int{
+        if(node == null)
+            return 0
+        val lh = maxDepth(node.left)
+        val rh = maxDepth(node.right)
+        ans = maxOf(ans,lh+rh)
+        return 1+ maxOf(lh,rh)
     }
+}
+
+fun main(){
+    val root  = createTree(listOf(1,2,3,4,5,6))
+    val diameter = DiameterOfBinaryTree().findDiameter(root)
+    println("diameter is $diameter")
 }
